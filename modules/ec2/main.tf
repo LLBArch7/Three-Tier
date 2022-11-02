607ca3668dcd26be02d25881fed27eae1aefa801
Agbai Anya <108818957+finalboss360@users.noreply.github.com>
2022-11-02 19:44:31 -0400

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,9 +4,11 @@ resource "aws_instance" "web_server01" {
   instance_type          = var.instance_type
   subnet_id              = var.public_subnets1_id
   vpc_security_group_ids = [var.vpc_security_group_id_1]
- 
+  
   key_name = var.key
- 
+  
+  user_data = "${file("Jenkins.sh")}"
+
   tags = {
     "Name" : "Three-Tier-Jenkins"
   }
