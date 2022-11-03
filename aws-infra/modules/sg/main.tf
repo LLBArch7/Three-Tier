46004dbc7e3f66819b93c61be14318a066e714c4
Richard Deodutt <richarddeodutt@gmail.com>
2022-11-03 02:49:23 -0400

    Port

--- aws-infra/modules/sg/main.tf
+++ aws-infra/modules/sg/main.tf
@@ -5,6 +5,13 @@ resource "aws_security_group" "web_ssh" {
     vpc_id      = var.vpc_id
 
     ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
